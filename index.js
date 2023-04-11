b53883bdf15ab5f51a0b6de84f161b9538e4efb6
unknown <nalanridlof@student.ub.ac.id>
2023-04-11 19:37:03 +0700

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("calculate");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click",  () => {
   let height = document.querySelector("#height").value;
   let weight = document.querySelector("#weight").value;
 
